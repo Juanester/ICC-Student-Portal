3fc4a6b42a27f798d9089b70e1b0b37ab9cfb25c
clark <tumbaliclarkd@gmail.com>
2024-01-17 15:23:39 +0800

    comment

--- application/views/V_Teacher_Dashboard.php
+++ application/views/V_Teacher_Dashboard.php
@@ -2,9 +2,15 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 ?>
 <!DOCTYPE html>
-
+<html lang="en">
 <html>
-
+<head>
+<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <base href="<?= base_url(); ?>">
+    <link rel="stylesheet" href="css\V_Teacher_Dashboard.css">
+</head>
 <body>
 
     <span>Welcome, <?= $teacher_info['last_name'] . ', ' . $teacher_info['first_name'] ?></span><br><br>
