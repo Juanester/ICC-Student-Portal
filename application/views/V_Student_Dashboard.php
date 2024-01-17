a2f4d6458f4ecb8499ce59ac95f90165666a089f
elmer21345 <elmersulitas27@gmail.com>
2024-01-17 14:30:38 +0800

    css

--- application/views/V_Student_Dashboard.php
+++ application/views/V_Student_Dashboard.php
@@ -68,7 +68,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
                 <button type="button" onclick="toggleProfileDropdown()" class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
                   <span class="absolute -inset-1.5"></span>
                   <span class="sr-only">Open user menu</span>
-                  <img class="h-8 w-8 rounded-full" src="try.webp" alt="">
+                  <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company">
                 </button>
               </div>
               <div id="myProfileDropdown" class="hidden absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
@@ -98,7 +98,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 <!-- ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- -->
 
-    <span>Welcomee, <?= $student_info['last_name'] . ', ' . $student_info['first_name'] ?></span><br><br>
+    <br>
+    <br>
     <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
         <div class="relative flex h-16 items-center justify-between">
         <table>
@@ -148,9 +149,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         </div>
         </div>
 
-        <form action="/C_Student_Dashboard/logout" method="post">
-            <input type="submit" value="Logout">
-        </form>
+        
 
 
 <!-- -------------------------------------------------------------------------------------JS FOR NAV BAR------------------------------------------------------------------------------------------------------------------------ -->
