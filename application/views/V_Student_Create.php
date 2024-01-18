5414363183c90802953f4b4c41d92601ff5a6d03
JeremyRostata <earlrostata274@gmail.com>
2024-01-18 15:06:44 +0800

    animations

--- application/views/V_Student_Create.php
+++ application/views/V_Student_Create.php
@@ -15,66 +15,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 </head>
 <body>
 
-<nav class="bg-gray-800">
-      <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div class="relative flex h-16 items-center justify-between">
-
-          <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-            
-            <div class="flex flex-shrink-0 items-center">
-            <img src="<?php echo base_url();?>images/iccl.webp" alt="" style="width: 50px;">
-            </div>
-
-            <div class="hidden sm:ml-6 sm:block">
-              <a class="flex space-x-4">
-                <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                <h1 class="text-gray-100 mt-5"><pre>  IMMACULADA CONCEPCION COLLEGE</pre></h1>  
-            </div>
-
-          </div>
-
-
-          <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            <button type="button" class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-              <span class="absolute -inset-1.5"></span>
-              <span class="sr-only">View notifications</span>
-              <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
-              </svg>
-            </button>
-    
-            <!-- Profile dropdown -->
-            
-            <div class="relative ml-3">
-              <div>
-                <button type="button" onclick="toggleProfileDropdown()" class="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800" id="user-menu-button" aria-expanded="false" aria-haspopup="true">
-                  <span class="absolute -inset-1.5"></span>
-                  <span class="sr-only">Open user menu</span>
-                  <img src="<?php echo base_url();?>images/pp.png" alt="" style="width: 55px;">
-                </button>
-              </div>
-              <div id="myProfileDropdown" class="hidden absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
-                <!-- Active: "bg-gray-100", Not Active: "" -->
-                      <span> <?= $teacher_info['last_name'] . ', ' . $teacher_info['first_name'] ?></span>
-                    <form action="/C_Teacher_Dashboard/logout" method="post">
-                    <input type="submit" value="Logout">
-                    </form>
-              </div>
-            </div>
-          
-          </div>
-        </div>
-      </div>
-      
-</nav>
-
-
-
-
-
-
-        
-
 
             <main>
                 <div class="logo">
@@ -89,7 +29,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <h2>CREATE STUDENT ACCOUNT</h2>   
         <form action=/C_Student_Management/studentCreate method="post" target="_self">
         <label for="student_id">Student:</label><br>
-        <select id="student_id" name="student_id">
+        <select class="text-gray-800" id="student_id" name="student_id">
             <option value="" hidden>-</option>
             <?php foreach ($student_list as $option) : ?>
             <option value="<?= $option['student_id'] ?>">
@@ -98,9 +38,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
             <?php endforeach; ?>
         </select><br>
         <label for="password">Password</label><br>
-        <input type="password" id="password" name="password" value=""><br><br>
+        <input class="text-gray-800" type="password" id="password" name="password" value=""><br><br>
 
-        <input type="submit" value="Create">
+        <input class="login_button hover:bg-blue-200 hover:text-gray-900" type="submit" value="Create">
         </form>
     </div>
 </main>
