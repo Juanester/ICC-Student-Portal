efa2be9a6f8f6f4647d15be595772daa2a0bf50f
elmer21345 <elmersulitas27@gmail.com>
2024-01-18 11:57:45 +0800

    mis

--- application/models/M_MIS_Dashboard.php
+++ application/models/M_MIS_Dashboard.php
@@ -1,22 +1,17 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class M_Employee_Login extends CI_Model{
+class M_Registrar_Dashboard extends CI_Model{
     public function __construct(){
         parent::__construct();
     }
-
-    public function fetchEmployeeId($employee_number, $password){
-        
-        $this->db->select('employee_user.employee_id');
-        $this->db->select('employee_user.access_role_id');
-        $this->db->from('employee_user');
-        $this->db->join('employee', 'employee_user.employee_id = employee.employee_id', 'left');
-        $this->db->where('employee.employee_number', $employee_number);
-        $this->db->where('employee_user.password', $password);
-        $result = $this->db->get();
-
-        return $result->result_array()[0];
+    
+    public function fetchRegistrarInfo($employee_id){
+        $this->db->select('first_name');
+        $this->db->select('last_name');
+        $this->db->from('employee');
+        $this->db->where('employee_id', $employee_id);
+        return $this->db->get()->result_array()[0];
     }
-
+    
 }
