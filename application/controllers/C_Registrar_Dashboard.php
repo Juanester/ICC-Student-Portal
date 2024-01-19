14758d8507f389b037580c14cfc0caab46df897d
sp1261 <jasper.garcia@denso-ten.com>
2024-01-19 19:02:00 +0800

    Upload function

--- application/controllers/C_Registrar_Dashboard.php
+++ application/controllers/C_Registrar_Dashboard.php
@@ -9,7 +9,7 @@ class C_Registrar_Dashboard extends CI_Controller {
         if(empty($_SESSION['employee_id'])){
             redirect('C_Employee_Login');
         }
-        
+        $this->load->library('excel');
         $this->load->model('M_Registrar_Dashboard');
 
     }
@@ -23,6 +23,31 @@ class C_Registrar_Dashboard extends CI_Controller {
             'registrar_info' => $registrar_info
         );
 		$this->load->view('V_Registrar_Dashboard', $data);
+    }
+
+    public function studentUpload(){
+
+        $objPHPExcel = PHPExcel_IOFactory::load($_FILES['fileToUpload']['tmp_name']);
+        $cell_collection = $objPHPExcel->getActiveSheet()->getCellCollection();
+ 
+
+        foreach ($cell_collection as $cell) {
+            $column = $objPHPExcel->getActiveSheet()->getCell($cell)->getColumn();
+            $row = $objPHPExcel->getActiveSheet()->getCell($cell)->getRow();
+            $data_value = $objPHPExcel->getActiveSheet()->getCell($cell)->getValue();
+         
+            //The header will/should be in row 1 only. of course, this can be modified to suit your need.
+            if ($row == 1) {
+                $header[$row][$column] = $data_value;
+            } else {
+                $arr_data[$row][$column] = $data_value;
+            }
+        }
+        
+
+        var_dump($header);
+        var_dump($arr_data);
+    
     }
 
     public function logout(){
--- application/views/V_Registrar_Dashboard.php
+++ application/views/V_Registrar_Dashboard.php
@@ -9,6 +9,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
     <span>Welcome, <?= $registrar_info['last_name'] . ', ' . $registrar_info['first_name'] ?></span><br><br>
 
+    <form action="/C_Registrar_Dashboard/studentUpload" method="post" enctype="multipart/form-data">
+        Select image to upload:
+        <input type="file" name="fileToUpload" id="fileToUpload">
+        <input type="submit" value="Upload Image" name="submit">
+    </form>
+
     <br><br>
     <form action="/C_Registrar_Dashboard/logout" method="post">
         <input type="submit" value="Logout">
